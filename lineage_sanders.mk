5e394996449a3a6f8a484b14150a9094cba385fc
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-11-12 15:48:17 -0700

    sanders: Set TARGET_BOOTANIMATION_HALF_RES to true
    * No apparent resolution difference
    * Much smoother animation
    * Much faster boot
    * What's not to like?
    
    Change-Id: I2119fe826146a3103da50b4862ad88f5950a97ec

--- lineage_sanders.mk
+++ lineage_sanders.mk
@@ -21,6 +21,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
+TARGET_BOOTANIMATION_HALF_RES := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_BRAND := motorola
